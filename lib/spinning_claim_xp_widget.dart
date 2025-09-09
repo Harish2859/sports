4c92014cec79926b9aeb0a7147c9cf19cea56230
Harish M <145277112+Harish2859@users.noreply.github.com>
2025-09-09 22:03:33 +0530

    n

--- lib/spinning_claim_xp_widget.dart
+++ lib/spinning_claim_xp_widget.dart
@@ -18,6 +18,7 @@ class SpinningClaimXPWidget extends StatefulWidget {
 class _SpinningClaimXPWidgetState extends State<SpinningClaimXPWidget>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
 
   @override
   void initState() {
@@ -26,8 +27,11 @@ class _SpinningClaimXPWidgetState extends State<SpinningClaimXPWidget>
       duration: Duration(seconds: 2),
       vsync: this,
     );
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 1.1).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
     if (!widget.claimed) {
-      _controller.repeat();
+      _controller.repeat(reverse: true);
     }
   }
 
@@ -37,7 +41,7 @@ class _SpinningClaimXPWidgetState extends State<SpinningClaimXPWidget>
     if (widget.claimed && !oldWidget.claimed) {
       _controller.stop();
     } else if (!widget.claimed && oldWidget.claimed) {
-      _controller.repeat();
+      _controller.repeat(reverse: true);
     }
   }
 
@@ -51,80 +55,15 @@ class _SpinningClaimXPWidgetState extends State<SpinningClaimXPWidget>
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: widget.claimed ? null : widget.onPressed,
-      child: AnimatedBuilder(
-        animation: _controller,
-        builder: (context, child) {
-          final angle = widget.claimed ? 0.0 : _controller.value * 2 * math.pi;
-          final isShowingFront = (angle % (2 * math.pi)) < math.pi;
-          
-          return Transform(
-            alignment: Alignment.center,
-            transform: Matrix4.identity()
-              ..setEntry(3, 2, 0.001)
-              ..rotateY(angle.toDouble()),
-            child: Container(
-              width: 120,
-              height: 120,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: widget.claimed ? Colors.grey : Colors.amber,
-                  width: 6,
-                ),
-                boxShadow: widget.claimed ? [] : [
-                  BoxShadow(
-                    color: Colors.amber.withOpacity(0.6),
-                    blurRadius: 15,
-                    spreadRadius: 3,
-                  ),
-                ],
-              ),
-              child: ClipOval(
-                child: isShowingFront
-                    ? ColorFiltered(
-                        colorFilter: widget.claimed 
-                          ? ColorFilter.mode(Colors.grey, BlendMode.saturation)
-                          : ColorFilter.mode(Colors.transparent, BlendMode.multiply),
-                        child: Image.asset(
-                          'assets/images/xp.png.png',
-                          fit: BoxFit.cover,
-                        ),
-                      )
-                    : Transform(
-                        alignment: Alignment.center,
-                        transform: Matrix4.rotationY(math.pi),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            gradient: RadialGradient(
-                              colors: widget.claimed 
-                                ? [Colors.grey[400]!, Colors.grey[600]!]
-                                : [Colors.amber[300]!, Colors.orange[600]!],
-                            ),
-                          ),
-                          child: Center(
-                            child: Text(
-                              '100\nXP',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                                shadows: [
-                                  Shadow(
-                                    color: Colors.black.withOpacity(0.5),
-                                    offset: Offset(1, 1),
-                                    blurRadius: 2,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-              ),
-            ),
-          );
-        },
+      child: ScaleTransition(
+        scale: _scaleAnimation,
+        child: Opacity(
+          opacity: widget.claimed ? 0.5 : 1.0,
+          child: Image.asset(
+            'assets/images/xp.png',
+            fit: BoxFit.contain,
+          ),
+        ),
       ),
     );
   }
